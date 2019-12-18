526b147f7e38f97ee3e5ef264f697bd6fb76c196
serhii1991 <serhiikleimovych>
2019-12-18 21:59:02 +0100

    added header, banner, form, services

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 $(function() {
 
-	
+	$('.placeholder').click(function() {
+		$(this).siblings('input').focus();
+	});
+	$('.form-control').focus(function() {
+		$(this).siblings('.placeholder').hide();
+	});
+	$('.form-control').blur(function() {
+		var $this = $(this);
+		if ($this.val().length == 0)
+			$(this).siblings('.placeholder').show();
+	});
+	$('.form-control').blur();
 
 });
